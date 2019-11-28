5178694a5801154b0a15ed28291fd35b20052052
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-11-28 11:27:52 +0330

    Setup migrations for "tweet" table

---
+++ db/migrations/20191030095516-tweets.js
@@ -0,0 +1,48 @@
+let dbm;
+let type;
+let seed;
+const fs = require('fs');
+const path = require('path');
+
+let Promise;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+  Promise = options.Promise;
+};
+
+exports.up = function (db) {
+  const filePath = path.join(__dirname, 'sqls', '20191030095516-tweets-up.sql');
+  return new Promise(((resolve, reject) => {
+    fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
+      if (err) return reject(err);
+      console.log(`received data: ${data}`);
+
+      resolve(data);
+    });
+  }))
+    .then((data) => db.runSql(data));
+};
+
+exports.down = function (db) {
+  const filePath = path.join(__dirname, 'sqls', '20191030095516-tweets-down.sql');
+  return new Promise(((resolve, reject) => {
+    fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
+      if (err) return reject(err);
+      console.log(`received data: ${data}`);
+
+      resolve(data);
+    });
+  }))
+    .then((data) => db.runSql(data));
+};
+
+exports._meta = {
+  version: 1,
+};
---
+++ db/migrations/sqls/20191030095516-tweets-down.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+DROP TABLE IF EXISTS `tweets`;
