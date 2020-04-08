a7f53548b0359b49fcc39c7f12e14506ec6f358c
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-04-08 18:44:32 +0430

    Move to root

--- knex.js
+++ knex.js
@@ -1,8 +1,8 @@
 const {
   NODE_ENV: env,
-} = require('../env');
+} = require('./env');
 
 const environment = env || 'development';
-const config = require('../knexfile.js')[environment];
+const config = require('./knexfile.js')[environment];
 
 module.exports = require('knex')(config);
