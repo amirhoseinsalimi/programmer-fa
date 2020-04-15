ba50c670a4e0c412eb0ffe83e5ec55687514d9ba
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-04-16 00:19:51 +0430

    Spit logs only if debug mode is enabled

--- src/bot/logger.ts
+++ src/bot/logger.ts
@@ -1,33 +1,50 @@
 const colorIt = require('color-it');
+import { isDebugModeEnabled } from './utils';
 
 export function logWarning(...args: string[]): void {
-  const l = args.length;
+  if (isDebugModeEnabled()) {
+    const l = args.length;
 
-  for (let i = 0; i < l; i++) {
-    console.log('' + colorIt(args[i]).orange());
+    for (let i = 0; i < l; i++) {
+      console.log('' + colorIt(args[i]).orange());
+    }
   }
+
+  return;
 }
 
 export function logError(...args: any): void {
-  const l = args.length;
+  if (isDebugModeEnabled()) {
+    const l = args.length;
 
-  for (let i = 0; i < l; i++) {
-    console.error('' + colorIt(args[i]).red());
+    for (let i = 0; i < l; i++) {
+      console.error('' + colorIt(args[i]).red());
+    }
   }
+
+  return;
 }
 
 export function logInfo(...args: any): void {
-  const l = args.length;
+  if (isDebugModeEnabled()) {
+    const l = args.length;
 
-  for (let i = 0; i < l; i++) {
-    console.log('' + colorIt(args[i]).belizeHole());
+    for (let i = 0; i < l; i++) {
+      console.log('' + colorIt(args[i]).belizeHole());
+    }
   }
+
+  return;
 }
 
 export function logSuccess(...args: any): void {
-  const l = args.length;
+  if (isDebugModeEnabled()) {
+    const l = args.length;
 
-  for (let i = 0; i < l; i++) {
-    console.log('' + colorIt(args[i]).green());
+    for (let i = 0; i < l; i++) {
+      console.log('' + colorIt(args[i]).green());
+    }
   }
+
+  return;
 }
