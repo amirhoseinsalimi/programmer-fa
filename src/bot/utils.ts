6c529704a4bfee049dc5d7c0b0325bc35079ec54
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-05-23 03:29:55 +0430

    Write functions using arrow syntax

--- src/bot/utils.ts
+++ src/bot/utils.ts
@@ -7,6 +7,11 @@ import { blackListedAccounts } from './black-listed-accounts';
 
 const knex = require('../../knex.js');
 
+interface Message {
+  message: String;
+  err?: any;
+}
+
 /**
  * Get an array of all occurrences of a substring in a string
  * @param {string} subStr - The sub-string to look for its occurrences
@@ -14,11 +19,11 @@ const knex = require('../../knex.js');
  * @param {boolean} [caseSensitive = false] - Case sensitivity matters?
  * @return {string[]}
  */
-export function getAllOccurrences(
+export const getAllOccurrences = (
   subStr: string,
   str: string,
   caseSensitive: boolean = false
-): number[] {
+): number[] => {
   const subStrLen = subStr.length;
 
   if (subStrLen === 0) {
@@ -40,7 +45,7 @@ export function getAllOccurrences(
   }
 
   return indices;
-}
+};
 
 /**
  * Remove words that are likely used in contexts other than programming.
@@ -49,7 +54,7 @@ export function getAllOccurrences(
  * @param {string} text - The text of the tweet
  * @return {string}
  */
-export function removeSuspiciousWords(text: string): string {
+export const removeSuspiciousWords = (text: string): string => {
   let lText = text.toLowerCase();
 
   suspiciousWords.forEach((word: string) => {
@@ -62,7 +67,7 @@ export function removeSuspiciousWords(text: string): string {
 
   // remove multiple contiguous spaces and return the string
   return lText.replace(/ +/g, ' ');
-}
+};
 
 /**
  * Remove URLs from the tweet.
@@ -72,32 +77,29 @@ export function removeSuspiciousWords(text: string): string {
  * @param {string} text - The text of the tweet
  * @return {string}
  */
-export function removeURLs(text: string): string {
+export const removeURLs = (text: string): string => {
   const urlRegex = /(https?:\/\/[^\s]+)/g;
 
   let lText = text.toLowerCase();
   lText.replace(urlRegex, '');
 
   return lText;
-}
+};
 
 /**
  * Checks whether a tweet has URL entities or not
  * @param tweet
  * @return boolean
  */
-export function hasURLs(tweet: any): boolean {
-  return tweet.entities.urls.length > 0;
-}
+export const hasURLs = (tweet: any): boolean => tweet.entities.urls.length > 0;
 
 /**
  * Whether a tweet is under 140 characters long or not
  * @param {*} tweet - The tweet object
  * @return {boolean}
  */
-export function isTweetExtended(tweet: any): boolean {
-  return tweet.truncated === true;
-}
+export const isTweetExtended = (tweet: any): boolean =>
+  tweet.truncated === true;
 
 /**
  * Whether a tweet is in Farsi or not.
@@ -105,48 +107,43 @@ export function isTweetExtended(tweet: any): boolean {
  * @param {*} tweet - The tweet object
  * @return boolean
  */
-export function isTweetFarsi(tweet: any): boolean {
-  return tweet.lang === 'fa';
-}
+export const isTweetFarsi = (tweet: any): boolean => tweet.lang === 'fa';
 
 /**
  *
  * @param {*} tweet - The tweet object
  * @return {boolean}
  */
-export function isTweetNotAReply(tweet: any): boolean {
+export const isTweetNotAReply = (tweet: any): boolean =>
   // Polyfill to check whether a tweet is a reply or not
-  return !tweet.in_reply_to_status_id && !tweet.in_reply_to_user_id;
-}
+  !tweet.in_reply_to_status_id && !tweet.in_reply_to_user_id;
 
 /**
  *
  * @param {*} tweet - The tweet object
  * @return {string}
  */
-export function getTweetFullText(tweet: any): string {
+export const getTweetFullText = (tweet: any): string =>
   // All tweets have a `text` property, but the ones having 140 or more
   // characters have `extended_tweet` property set to `true` and an extra
   // `extended_tweet` property containing the actual tweet's text under
   // `full_text`. For tweets which are not truncated the former `text` is
   // enough.
-  return isTweetExtended(tweet) ? tweet.extended_tweet.full_text : tweet.text;
-}
+  isTweetExtended(tweet) ? tweet.extended_tweet.full_text : tweet.text;
 
 /**
  *
  * @param {*} tweet - The tweet object
  * @return {string[]}
  */
-export function getTweetHashtags(tweet: any): string[] {
-  return tweet.entities.hashtags;
-}
+export const getTweetHashtags = (tweet: any): string[] =>
+  tweet.entities.hashtags;
 
 /**
  * Whether the environment is in debug mode or not
  * @return {boolean}
  */
-export function isDebugModeEnabled(): boolean {
+export const isDebugModeEnabled = (): boolean => {
   const environment = env || 'development';
 
   if (environment === 'production') {
@@ -154,32 +151,34 @@ export function isDebugModeEnabled(): boolean {
   } else {
     return debugMode !== 'false';
   }
-}
+};
 
 /**
  * Retweet the passed tweet by the given `id`
  * @param {number} id - Tweet ID
  * @return {Promise}
  */
-export function retweet(id: number): Promise<any> {
-  return new Promise((resolve, reject) => {
+export const retweet = (id: number): Promise<Message> =>
+  new Promise((resolve, reject) => {
     T.post('statuses/retweet/:id', { id: id.toString() }, (err) => {
       if (err) {
-        return reject(err);
+        return reject({
+          message: `Failed to retweet the tweet ${id}`,
+          err: err,
+        });
       }
 
       resolve({ message: 'Tweet retweeted successfully' });
     });
   });
-}
 
 /**
  * Favourite/Like the passed tweet by the given `id`
  * @param {number} id - Tweet ID
  * @return {Promise}
  */
-export function favourite(id: number): Promise<any> {
-  return new Promise((resolve, reject) => {
+export const favourite = (id: number): Promise<Message> =>
+  new Promise((resolve, reject) => {
     T.post('/favorites/create', { id: id.toString() }, (err) => {
       if (err) {
         return reject(err);
@@ -188,14 +187,13 @@ export function favourite(id: number): Promise<any> {
       resolve({ message: 'Tweet favourited successfully' });
     });
   });
-}
 
 /**
  * Store the given tweet in the database
  * @param {*} tweet - The tweet object
  * @return {Promise}
  */
-export function store(tweet: any) {
+export const store = (tweet: any): Promise<Message> => {
   const {
     in_reply_to_status_id,
     in_reply_to_user_id,
@@ -223,16 +221,15 @@ export function store(tweet: any) {
         reject(err);
       });
   });
-}
+};
 
 /**
  * Check if the user is not in the blacklist
  * @param {string} userId
  * @return {boolean}
  */
-export function isNotBlackListed(userId: string): boolean {
-  return !blackListedAccounts.includes(userId);
-}
+export const isNotBlackListed = (userId: string): boolean =>
+  !blackListedAccounts.includes(userId);
 
 /**
  * Returns the number of intersections b/w two arrays
@@ -240,27 +237,20 @@ export function isNotBlackListed(userId: string): boolean {
  * @param {string[]} arr2
  * @return {number}
  */
-export function getIntersectionCount(arr1: string[], arr2: string[]): number {
-  return [...new Set(arr1)].filter((v) => arr2.includes(v)).length;
-}
+export const getIntersectionCount = (arr1: string[], arr2: string[]): number =>
+  [...new Set(arr1)].filter((v) => arr2.includes(v)).length;
 
 /**
  * Check if a tweet has 4 hashtags or less. See it as an ad-blocker.
  * @param {*} tweet - The tweet object
  * @return {boolean}
  */
-export function hasLessThanFourHashtags(tweet: any): boolean {
-  return (
-    getAllOccurrences('#', getTweetFullText(tweet), true).length <= 4 &&
-    tweet.entities.hashtags.length <= 4
-  );
-}
-
+export const hasLessThanFourHashtags = (tweet: any): boolean =>
+  getAllOccurrences('#', getTweetFullText(tweet), true).length <= 4 &&
+  tweet.entities.hashtags.length <= 4;
 /**
  * Check if a tweet is retweeted by ME or not
  * @param {*} tweet - The tweet object
  * @return {boolean}
  */
-export function isRetweeted(tweet: any): boolean {
-  return tweet.retweeted;
-}
+export const isRetweeted = (tweet: any): boolean => tweet.retweeted;
