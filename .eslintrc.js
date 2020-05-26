b03969e6ea90ffa1d37c0face2cdb87f63c8cbde
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-05-26 17:45:35 +0430

    Update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,16 @@ module.exports = {
   rules: {
     'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.ts'] }],
     '@typescript-eslint/indent': [2, 2],
+    '@typescript-eslint/camelcase': [0, 0],
+    '@typescript-eslint/no-var-requires': [0, 0],
+    'no-param-reassign': [0, 0],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "ts": "never",
+      }
+    ],
   },
 };
