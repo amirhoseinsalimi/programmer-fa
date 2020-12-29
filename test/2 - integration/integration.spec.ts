a203fc60cb27ad92d5d29d52f62b68227b6a8876
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-12-29 10:57:50 +0330

    Expect tweet ids to be string

--- test/2 - integration/integration.spec.ts
+++ test/2 - integration/integration.spec.ts
@@ -86,15 +86,15 @@ describe('Integration Tests', () => {
     done();
   });
 
-  it('should return the `id` of valid tweets or `0` for invalid tweets', (done) => {
+  it('should return the `id` of valid tweets or "0" for invalid tweets', (done) => {
     tweets.forEach(
       async (tweet: any) => {
         if (tweet.tweetIsValid) {
           expect(await onTweet(tweet))
-            .to.be.not.equal(0);
+            .to.be.not.equal('0');
         } else {
           expect(await onTweet(tweet))
-            .to.be.equal(0);
+            .to.be.equal('0');
         }
       },
     );
