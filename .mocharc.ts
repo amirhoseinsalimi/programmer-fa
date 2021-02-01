7d6af18dd00f4d9ea1fa9350d8b72eee332e8552
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-02-01 08:55:46 +0330

    Fix .gitignore

--- .mocharc.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  spec: 'test/**/*.spec.ts',
-};
--- src/knex-export.ts
+++
@@ -1,8 +0,0 @@
-import * as Knex from 'knex';
-
-import envs from './env';
-
-const environment = envs.NODE_ENV || 'development';
-const config = require('./knexfile')[environment];
-
-export default Knex(config);
