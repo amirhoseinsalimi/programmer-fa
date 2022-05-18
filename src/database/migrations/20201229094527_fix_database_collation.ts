5855505489ff7ee74c33934554f626f2f24e0599
Amir Hosein Salimi <a.salimi@karboom.io>
2022-05-18 19:08:06 +0430

    style: use template literal

--- src/database/migrations/20201229094527_fix_database_collation.ts
+++ src/database/migrations/20201229094527_fix_database_collation.ts
@@ -5,20 +5,24 @@ import envs from '../../env';
 
 const TABLE_NAME = 'users';
 
-export async function up({schema}: Knex): Promise<any> {
+export async function up({ schema }: Knex): Promise<any> {
   const tableExists = await schema.hasTable(TABLE_NAME);
 
   if (tableExists) {
-    return schema.raw('ALTER DATABASE `' + envs.DB_NAME + '` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;');
+    return schema.raw(
+      `ALTER DATABASE \`${envs.DB_NAME}\` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;`,
+    );
   }
 }
 
-export async function down({schema}: Knex): Promise<any> {
+export async function down({ schema }: Knex): Promise<any> {
   const tableExists = await schema.hasTable(TABLE_NAME);
 
   if (tableExists) {
-    return schema.raw('ALTER DATABASE `' + envs.DB_NAME + '` CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;');
+    return schema.raw(
+      `ALTER DATABASE \`${envs.DB_NAME}\` CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;`,
+    );
   }
 }
 
-exports.config = {transaction: false};
+exports.config = { transaction: false };
